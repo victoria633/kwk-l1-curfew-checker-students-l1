052726eec3c514a2f26c1a4a165bf5c258e0df6a
victoria633 <victoria6411@hotmail.com>
2018-06-05 21:21:43 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,12 +1,23 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time>=11 
+    puts "Your in trouble!Better get home quick"
+  end
 end
+puts simple_curfew_checker(11)
+
+
 
 def curfew_checker(time)
-  # code goes here
+   if time==12
+  puts "Your in trouble!Better get home quick"
+   end
 end
+puts simple_curfew_checker(11)
+
 
 def complex_curfew_checker(time)
+ if time<=11
+   puts
   # code goes here
 end
 
